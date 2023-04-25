f59dd71ed03458f430d5faa001a9d77680cad693
rosemutai <chepngetichrose2030@gmail.com>
2023-04-25 09:13:03 +0300

    Style Button component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import Display from './Display';
+import Button from './Button';
+
+const btnValues = [
+  ['AC', '+/-', '%', '/'],
+  [7, 8, 9, 'X'],
+  [4, 5, 6, '-'],
+  [1, 2, 3, '+'],
+  [0, '.', '='],
+];
 
 const Calculator = () => (
-  <div>
-    <h1>Calculator</h1>
+  <div className="calc-homepage w-1/4 my-16 mx-auto bg-gray-200">
+    <Display />
+    <div className="grid grid-cols-4 grid-rows-5 gap-1">
+      {btnValues.flat().map((item, index) => (
+        <div key={item} className={`${index === 16 ? 'col-span-2' : 'col-span-1'}`}>
+          <Button value={item} />
+        </div>
+      ))}
+    </div>
   </div>
 );
 
