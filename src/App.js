a26394eef9f9344c8ad7f741a8b172d8e979ae18
rosemutai <chepngetichrose2030@gmail.com>
2023-05-02 16:17:48 +0300

    Add project routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Calculator from './components/Calculator';
-import Quote from './components/Quote';
+import { Routes, Route } from 'react-router-dom';
+import Calculator from './routes/Calculator';
+import Navbar from './components/Navbar';
+import Quote from './routes/Quote';
+import Home from './routes/Home';
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
-      <Quote />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="quotes" element={<Quote />} />
+      </Routes>
     </div>
   );
 }
